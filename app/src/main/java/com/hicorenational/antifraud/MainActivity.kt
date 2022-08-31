4e4c48163c0639067f5075117ffdf6a3f4f831fd
iMorning <morningos@foxmail.com>
2022-08-31 21:10:05 +0800

    Initial commit

---
+++ app/src/main/java/com/hicorenational/antifraud/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.hicorenational.antifraud
+
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        MainScope().launch(Dispatchers.IO) {
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.apply {
+                setMessage("很抱歉，${getString(R.string.app_name)}已停止运行")
+                setPositiveButton("关闭应用") { _, _ -> this@MainActivity.finish() }
+            }
+            delay(3000)
+            withContext(Dispatchers.Main) {
+                builder.show()
+            }
+        }
+    }
+}
